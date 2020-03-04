caef959f935fcc98562218483d3a42a938dc1af1
Alex Tarragó <alex@dribba.com>
2020-03-04 19:02:25 +0100

    refactor

---
+++ app/src/main/java/edu/url/salle/eric/macia/bubblefy/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.url.salle.eric.macia.bubblefy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
