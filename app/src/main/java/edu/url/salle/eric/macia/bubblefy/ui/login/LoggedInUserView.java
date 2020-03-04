d44fc155c77ad1bab27ae981ddadcc70b2a0762c
guillesabate <guillesabate@gmail.com>
2020-03-04 19:25:01 +0100

    Hola papus nou template del login 2

---
+++ app/src/main/java/edu/url/salle/eric/macia/bubblefy/ui/login/LoggedInUserView.java
@@ -0,0 +1,17 @@
+package edu.url.salle.eric.macia.bubblefy.ui.login;
+
+/**
+ * Class exposing authenticated user details to the UI.
+ */
+class LoggedInUserView {
+    private String displayName;
+    //... other data fields that may be accessible to the UI
+
+    LoggedInUserView(String displayName) {
+        this.displayName = displayName;
+    }
+
+    String getDisplayName() {
+        return displayName;
+    }
+}
