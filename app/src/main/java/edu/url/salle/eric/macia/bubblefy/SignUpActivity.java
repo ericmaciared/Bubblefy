7d14b04b3ae5739f687b5459b7f5b39a729abc7f
Alex Tarragó <alex@dribba.com>
2020-03-04 19:15:37 +0100

    refactor

---
+++ app/src/main/java/edu/url/salle/eric/macia/bubblefy/SignUpActivity.java
@@ -0,0 +1,14 @@
+package edu.url.salle.eric.macia.bubblefy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
