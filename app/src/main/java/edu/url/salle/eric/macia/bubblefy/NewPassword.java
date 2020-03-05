0e02629d23ec09d678c560c3f2d282ef662b8626
David Ruano <davidruano101@gmail.com>
2020-03-06 00:46:10 +0100

    He creat el disseny de noves Acivities de Login (falta relacionar-les unes amb les altes mitjançant les classes. - David

---
+++ app/src/main/java/edu/url/salle/eric/macia/bubblefy/NewPassword.java
@@ -0,0 +1,14 @@
+package edu.url.salle.eric.macia.bubblefy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_password);
+    }
+}
