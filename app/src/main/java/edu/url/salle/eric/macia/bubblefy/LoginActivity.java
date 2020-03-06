ea85af6231c244030914b82d8df2c1b5c73690a6
Eric Macià <ericmaciared@gmail.com>
2020-03-06 15:22:23 +0100

    Linked activity login amb sign up page. Update del login activity.

--- app/src/main/java/edu/url/salle/eric/macia/bubblefy/LoginActivity.java
+++ app/src/main/java/edu/url/salle/eric/macia/bubblefy/LoginActivity.java
@@ -2,13 +2,46 @@ package edu.url.salle.eric.macia.bubblefy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button loginButton;
+    private Button signupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = (Button) findViewById(R.id.LoginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //loadHomePageActivity();
+            }
+        });
+
+        signupButton = (Button) findViewById(R.id.SignUpButton);
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadSignUpPageActivity();
+            }
+        });
     }
+
+    protected void loadHomePageActivity(){
+        /*Intent intent = new Intent(this, null.class);
+        startActivity(intent);*/
+    }
+
+    protected void loadSignUpPageActivity(){
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+    }
+
 }
