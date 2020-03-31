2bc4219685e6540a7f22ed020ec3f1a12308c236
David Ruano <davidruano101@gmail.com>
2020-03-31 22:49:48 +0200

    Search button implemented (just tracks)

--- app/src/main/java/edu/url/salle/eric/macia/bubblefy/controller/activity/SearchActivity.java
+++ app/src/main/java/edu/url/salle/eric/macia/bubblefy/controller/activity/SearchActivity.java
@@ -1,26 +1,42 @@
 package edu.url.salle.eric.macia.bubblefy.controller.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 import edu.url.salle.eric.macia.bubblefy.R;
+import edu.url.salle.eric.macia.bubblefy.controller.adapters.TrackListAdapter;
+import edu.url.salle.eric.macia.bubblefy.model.Playlist;
 import edu.url.salle.eric.macia.bubblefy.model.Search;
+import edu.url.salle.eric.macia.bubblefy.model.Track;
+import edu.url.salle.eric.macia.bubblefy.restapi.callback.PlaylistCallback;
 import edu.url.salle.eric.macia.bubblefy.restapi.callback.SearchCallback;
+import edu.url.salle.eric.macia.bubblefy.restapi.manager.PlaylistManager;
 import edu.url.salle.eric.macia.bubblefy.restapi.manager.SearchManager;
 
 
-public class SearchActivity extends AppCompatActivity implements SearchCallback, RadioGroup.OnCheckedChangeListener {
+public class SearchActivity extends AppCompatActivity implements SearchCallback, RadioGroup.OnCheckedChangeListener, PlaylistCallback {
 
     RadioGroup radioGroup;
     RadioButton radioButton1;
     RadioButton radioButton2;
     RadioButton radioButton3;
     TextView textToShow;
-    private Search mSearch;
+    private RecyclerView mRecyclerView;
+    private ArrayList<Track> mTracks;
+    private Button buttonLogin;
+    private EditText searchText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,17 +47,43 @@ public class SearchActivity extends AppCompatActivity implements SearchCallback,
         radioButton2 = findViewById(R.id.radio_playlists);
         radioButton3 = findViewById(R.id.radio_users);
         textToShow = findViewById(R.id.textprova);
-
+        buttonLogin = findViewById(R.id.search_button);
         radioGroup.setOnCheckedChangeListener(this);
+        searchText = findViewById(R.id.search_text);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSearch();
+            }
+        });
 
-        SearchManager.getInstance(this).getSearchResult("Hola", this);
+        initViews();
     }
 
+    public void getSearch(){
+        SearchManager.getInstance(this).getSearchResult((searchText.getText()).toString(), this);
+    }
 
     @Override
     public void onSearchReceived(Search search) {
-        mSearch = search;
-        System.out.println(mSearch);
+        mTracks = (ArrayList) search.getTracks();
+        TrackListAdapter adapter = new TrackListAdapter(this, mTracks);
+        mRecyclerView.setAdapter(adapter);
+
+        Playlist playlist = new Playlist();
+        playlist.setName("Playlist");
+        playlist.setPublicAccessible(true);
+        PlaylistManager manager = new PlaylistManager(this);
+        manager.createPlaylist(playlist, this);
+    }
+
+
+    private void initViews() {
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        LinearLayoutManager manager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
+        TrackListAdapter adapter = new TrackListAdapter(this, null);
+        mRecyclerView.setLayoutManager(manager);
+        mRecyclerView.setAdapter(adapter);
     }
 
     @Override
@@ -62,5 +104,15 @@ public class SearchActivity extends AppCompatActivity implements SearchCallback,
             textToShow.setText("fina ara");}
         else
             textToShow.setText("adeu");
+    }
+
+    @Override
+    public void onPlaylistCreated(Playlist playlist) {
+
+    }
+
+    @Override
+    public void onPlaylistFailure(Throwable throwable) {
+
     }
 }
