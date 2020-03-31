5243b2b8d0a5147f5cc564dd07663b8c12815887
David Ruano <davidruano101@gmail.com>
2020-03-31 21:38:46 +0200

    Search bar and top buttons implemented

--- app/src/main/java/edu/url/salle/eric/macia/bubblefy/controller/activity/SearchActivity.java
+++ app/src/main/java/edu/url/salle/eric/macia/bubblefy/controller/activity/SearchActivity.java
@@ -3,6 +3,9 @@ package edu.url.salle.eric.macia.bubblefy.controller.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import edu.url.salle.eric.macia.bubblefy.R;
 import edu.url.salle.eric.macia.bubblefy.model.Search;
@@ -10,16 +13,30 @@ import edu.url.salle.eric.macia.bubblefy.restapi.callback.SearchCallback;
 import edu.url.salle.eric.macia.bubblefy.restapi.manager.SearchManager;
 
 
-public class SearchActivity extends AppCompatActivity implements SearchCallback {
+public class SearchActivity extends AppCompatActivity implements SearchCallback, RadioGroup.OnCheckedChangeListener {
 
+    RadioGroup radioGroup;
+    RadioButton radioButton1;
+    RadioButton radioButton2;
+    RadioButton radioButton3;
+    TextView textToShow;
     private Search mSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        radioGroup = findViewById(R.id.group_buttons);
+        radioButton1 = findViewById(R.id.radio_song);
+        radioButton2 = findViewById(R.id.radio_playlists);
+        radioButton3 = findViewById(R.id.radio_users);
+        textToShow = findViewById(R.id.textprova);
+
+        radioGroup.setOnCheckedChangeListener(this);
+
         SearchManager.getInstance(this).getSearchResult("Hola", this);
     }
+
 
     @Override
     public void onSearchReceived(Search search) {
@@ -35,5 +52,15 @@ public class SearchActivity extends AppCompatActivity implements SearchCallback 
     @Override
     public void onFailure(Throwable throwable) {
 
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        if(radioButton1.isChecked()){
+            textToShow.setText("hola");}
+        else if(radioButton2.isChecked()){
+            textToShow.setText("fina ara");}
+        else
+            textToShow.setText("adeu");
     }
 }
