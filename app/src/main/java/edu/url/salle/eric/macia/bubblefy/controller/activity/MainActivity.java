e6731d98b65c5c7a26a2dc044e3bfee4a82c17e8
Eric Macià <ericmaciared@gmail.com>
2020-04-12 11:47:36 +0200

    Added bubble menu on mainActivity.

--- app/src/main/java/edu/url/salle/eric/macia/bubblefy/controller/activity/MainActivity.java
+++ app/src/main/java/edu/url/salle/eric/macia/bubblefy/controller/activity/MainActivity.java
@@ -1,16 +1,42 @@
 package edu.url.salle.eric.macia.bubblefy.controller.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.igalata.bubblepicker.BubblePickerListener;
+import com.igalata.bubblepicker.adapter.BubblePickerAdapter;
+import com.igalata.bubblepicker.model.PickerItem;
+import com.igalata.bubblepicker.rendering.BubblePicker;
+
+import org.jetbrains.annotations.NotNull;
+
+
+import java.util.ArrayList;
 
 import edu.url.salle.eric.macia.bubblefy.R;
+import edu.url.salle.eric.macia.bubblefy.model.Track;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BubblePicker bubblePicker;
+
+    private String[] name = {
+            "Colores",
+            "YHLQMDLG",
+            "Blue",
+            "Chill",
+            "Party"
+    };
+
+    private RecyclerView recyclerView;
+    private ArrayList<Track> mTracks;
     private ImageButton ibtnHome;
     private ImageButton ibtnSearch;
     private ImageButton ibtnProfile;
@@ -25,6 +51,45 @@ public class MainActivity extends AppCompatActivity {
         ibtnProfile = (ImageButton) findViewById(R.id.profile_button);
 
         initViews();
+        initBubblePicker();
+
+        bubblePicker.setCenterImmediately(true);
+        bubblePicker.setKeepScreenOn(true);
+    }
+
+    private void initBubblePicker() {
+        bubblePicker = (BubblePicker) findViewById(R.id.picker);
+
+        bubblePicker.setAdapter(new BubblePickerAdapter() {
+            @Override
+            public int getTotalCount() {
+                return name.length;
+            }
+
+            @NotNull
+            @Override
+            public PickerItem getItem(int i) {
+                PickerItem item = new PickerItem();
+                //item.setColor(120);
+                //item.setTitle(name[i]);
+                //item.setTextColor(255);
+                //item.setOverlayAlpha(1);
+                return item;
+            }
+        });
+
+        bubblePicker.setListener(new BubblePickerListener() {
+            @Override
+            public void onBubbleSelected(@NotNull PickerItem pickerItem) {
+                Toast.makeText(MainActivity.this, "You have clicked "+
+                        pickerItem.getTitle(), Toast.LENGTH_SHORT);
+            }
+
+            @Override
+            public void onBubbleDeselected(@NotNull PickerItem pickerItem) {
+
+            }
+        });
     }
 
     private void initViews(){
@@ -46,5 +111,17 @@ public class MainActivity extends AppCompatActivity {
     private void loadSearchScreen() {
         Intent intent = new Intent(this, SearchActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        bubblePicker.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        bubblePicker.onPause();
     }
 }
