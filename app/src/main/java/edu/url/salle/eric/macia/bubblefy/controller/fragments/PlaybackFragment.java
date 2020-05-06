c458b304797fdac1f654562a8fca6e411585f5a1
Eric Macià <ericmaciared@gmail.com>
2020-05-06 20:01:18 +0200

    Added response for playback buttons.

--- app/src/main/java/edu/url/salle/eric/macia/bubblefy/controller/fragments/PlaybackFragment.java
+++ app/src/main/java/edu/url/salle/eric/macia/bubblefy/controller/fragments/PlaybackFragment.java
@@ -1,5 +1,6 @@
 package edu.url.salle.eric.macia.bubblefy.controller.fragments;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,12 +17,19 @@ import edu.url.salle.eric.macia.bubblefy.R;
 import edu.url.salle.eric.macia.bubblefy.controller.activity.MainActivity;
 
 public class PlaybackFragment extends Fragment {
+    private static final String PLAY_VIEW = "PlayIcon";
+    private static final String STOP_VIEW = "StopIcon";
+
     private ImageView ivTrackImg;
     private TextView tvTrackTitle;
     private TextView tvTrackAuthor;
 
     private ImageButton ibArrowDown;
     private ImageButton ibPlayPause;
+    private ImageButton ibRandom;
+    private ImageButton ibRepeat;
+    private ImageButton ibNextSong;
+    private ImageButton ibPreviousSong;
 
     public PlaybackFragment(int activity) {
     }
@@ -56,6 +64,59 @@ public class PlaybackFragment extends Fragment {
         });
 
 
+        this.ibPlayPause = (ImageButton) v.findViewById(R.id.playback_play_pause);
+        ibPlayPause.setTag(MainActivity.btnPlayStop.getTag());
+        if (ibPlayPause.getTag().equals(PLAY_VIEW)) {
+            ibPlayPause.setImageResource(R.drawable.ic_play3);
+        } else {
+            ibPlayPause.setImageResource(R.drawable.ic_pause2);
+        }
+        ibPlayPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ibPlayPause.getTag().equals(PLAY_VIEW)) {
+                    MainActivity.mediaPlayer.start();
+                    ibPlayPause.setTag(STOP_VIEW);
+                    ibPlayPause.setImageResource(R.drawable.ic_pause2);
+                } else {
+                    MainActivity.mediaPlayer.pause();
+                    ibPlayPause.setTag(PLAY_VIEW);
+                    ibPlayPause.setImageResource(R.drawable.ic_play3);
+                }
+            }
+        });
+
+        this.ibNextSong = (ImageButton) v.findViewById(R.id.playback_forward);
+        ibNextSong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        this.ibPreviousSong = (ImageButton) v.findViewById(R.id.playback_backward);
+        ibPreviousSong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        this.ibRandom = (ImageButton) v.findViewById(R.id.playback_random);
+        ibRandom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        this.ibRepeat = (ImageButton) v.findViewById(R.id.playback_repeat);
+        ibRepeat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
 
