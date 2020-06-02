23c9cc528bac44b5b10a543df43483d2f8e6d88b
guillesabate <guillesabate@gmail.com>
2020-06-03 01:12:27 +0200

    lil tweaks sorry dont mind these

--- app/src/main/java/edu/url/salle/eric/macia/bubblefy/controller/fragments/HomeFragment.java
+++ app/src/main/java/edu/url/salle/eric/macia/bubblefy/controller/fragments/HomeFragment.java
@@ -1,5 +1,7 @@
 package edu.url.salle.eric.macia.bubblefy.controller.fragments;
 
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,12 +9,15 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.igalata.bubblepicker.BubblePickerListener;
 import com.igalata.bubblepicker.adapter.BubblePickerAdapter;
+import com.igalata.bubblepicker.model.BubbleGradient;
 import com.igalata.bubblepicker.model.PickerItem;
 import com.igalata.bubblepicker.rendering.BubblePicker;
 
@@ -20,30 +25,33 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import edu.url.salle.eric.macia.bubblefy.R;
 import edu.url.salle.eric.macia.bubblefy.controller.activity.MainActivity;
 import edu.url.salle.eric.macia.bubblefy.controller.adapters.BubbleTrackListAdapter;
 import edu.url.salle.eric.macia.bubblefy.model.Confirmation;
+import edu.url.salle.eric.macia.bubblefy.model.Genre;
+import edu.url.salle.eric.macia.bubblefy.model.Playlist;
 import edu.url.salle.eric.macia.bubblefy.model.Track;
+import edu.url.salle.eric.macia.bubblefy.restapi.callback.GenreCallback;
 import edu.url.salle.eric.macia.bubblefy.restapi.callback.TrackCallback;
+import edu.url.salle.eric.macia.bubblefy.restapi.manager.GenreManager;
+import edu.url.salle.eric.macia.bubblefy.restapi.manager.ImageManager;
 import edu.url.salle.eric.macia.bubblefy.restapi.manager.TrackManager;
 
-public class HomeFragment extends Fragment implements TrackCallback {
+public class HomeFragment extends Fragment implements TrackCallback, GenreCallback {
 
+    //Bubble Picker
     private BubblePicker bubblePicker;
+    private ArrayList<Genre> mGenres;
+    private TypedArray colors;
 
-    private String[] name = {
-            "Colores",
-            "YHLQMDLG",
-            "Blue",
-            "Chill",
-            "Party"
-    };
-
+    private View v;
     private RecyclerView recyclerView;
     private BubbleTrackListAdapter mAdapter;
     private ArrayList<Track> mTracks;
+
 
     @Nullable
     @Override
@@ -51,19 +59,20 @@ public class HomeFragment extends Fragment implements TrackCallback {
         View v = inflater.inflate(R.layout.fragment_home, container, false);
 
         initListenAgain(v);
-        initBubblePicker(v);
 
         if (MainActivity.mediaPlayer.isPlaying()){
             MainActivity.showNavigation(true);
             MainActivity.showPlayback(true);
         }
 
+        colors = getResources().obtainTypedArray(R.array.colors);
+
         return v;
     }
 
     private void initListenAgain(View v) {
         recyclerView = (RecyclerView) v.findViewById(R.id.recyclerView);
-
+        bubblePicker = (BubblePicker) v.findViewById(R.id.picker);
         LinearLayoutManager manager = new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL,
                 false);
         mAdapter = new BubbleTrackListAdapter(getActivity(), null);
@@ -72,6 +81,7 @@ public class HomeFragment extends Fragment implements TrackCallback {
         recyclerView.setAdapter(mAdapter);
 
         TrackManager.getInstance(getActivity()).getUserLikedTracks(this);
+        GenreManager.getInstance(getActivity()).getAllGenres(this);
 
         mAdapter.setOnItemClickListener(new BubbleTrackListAdapter.OnItemClickListener() {
             @Override
@@ -82,30 +92,35 @@ public class HomeFragment extends Fragment implements TrackCallback {
     }
 
     private void initBubblePicker(View v) {
-        bubblePicker = (BubblePicker) v.findViewById(R.id.picker);
-
         bubblePicker.setAdapter(new BubblePickerAdapter() {
             @Override
             public int getTotalCount() {
-                return name.length;
+                return mGenres.size();
             }
 
             @NotNull
             @Override
             public PickerItem getItem(int i) {
+                Genre genre = mGenres.get(i);
                 PickerItem item = new PickerItem();
-                item.setTitle(name[i]);
+                item.setTitle(genre.getName());
+                item.setGradient(new BubbleGradient(colors.getColor((i * 2) % 8, 0),
+                        colors.getColor((i * 2) % 8 + 1, 0), BubbleGradient.VERTICAL));
+                item.setTextColor(ContextCompat.getColor(getContext(),android.R.color.white));
+                item.setTypeface(Typeface.DEFAULT);
                 return item;
             }
-
         });
+
+
 
         bubblePicker.setListener(new BubblePickerListener() {
             @Override
             public void onBubbleSelected(@NotNull PickerItem pickerItem) {
                 if (pickerItem.isSelected()){
-                    pickerItem.setTitle("TEST");
+                    goToGenre(pickerItem);
                 }
+                goToGenre(pickerItem);
             }
 
             @Override
@@ -124,13 +139,31 @@ public class HomeFragment extends Fragment implements TrackCallback {
     @Override
     public void onResume() {
         super.onResume();
-        bubblePicker.onResume();
+        bubblePicker.onPause();
     }
 
     @Override
     public void onPause() {
         super.onPause();
         bubblePicker.onPause();
+    }
+
+    public void goToGenre(PickerItem pickerItem){
+        Genre selectedGenre;
+        for(int i = 0; i < mGenres.size(); i++){
+            if(pickerItem.getTitle() == mGenres.get(i).getName()){
+                selectedGenre = mGenres.get(i);
+                /*
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+                transaction.replace(R.id.fragment_container, new PlaylistFragment(selectedGenre));
+                transaction.addToBackStack(null);
+                transaction.commit();
+                break;
+                 */
+            }
+        }
+
     }
 
     @Override
@@ -156,7 +189,6 @@ public class HomeFragment extends Fragment implements TrackCallback {
     @Override
     public void onUserLikedTracksReceived(List<Track> tracks) {
         mTracks = (ArrayList<Track>) tracks;
-
         mAdapter = new BubbleTrackListAdapter(getActivity(), mTracks);
         recyclerView.setAdapter(mAdapter);
     }
@@ -188,6 +220,23 @@ public class HomeFragment extends Fragment implements TrackCallback {
 
     @Override
     public void onFailure(Throwable throwable) {
+
+    }
+
+    public View getV() {
+        return v;
+    }
+
+    @Override
+    public void onGenresReceive(ArrayList<Genre> genres) {
+        mGenres = new ArrayList<>();
+        mGenres.addAll(genres);
+        initBubblePicker(getV());
+        bubblePicker.onResume();
+    }
+
+    @Override
+    public void onTracksByGenre(ArrayList<Track> tracks) {
 
     }
 }
